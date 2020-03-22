512b1e6446e36522b1bf3f4c9de20285a480d16e
alovett <alovett1215@gmail.com>
2020-03-22 07:30:58 -0600

    array and booleans intro

--- index.php
+++ index.php
@@ -19,4 +19,27 @@ $animals = [
 
 ];
 
+
+
+$person = [
+    'age' => 31,
+    'hair' => 'blonde',
+    'career' => 'web developer'
+];
+
+
+$person['name'] = 'SCP-001';
+
+unset($person['age']);
+
+
+// die(var_dump($person));
+
+$task =[
+    'title' => 'get nano chips removed',
+    'due' => 'today',
+    'assigned_to' => 'Al',
+    'completed' => false 
+]; // title, due, assigned_to, completed
+
 require 'index.view.php';
