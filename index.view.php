f96ffbc03cba6a1df4bfab7d423efc633b456608
alovett <alovett1215@gmail.com>
2020-03-26 03:21:08 -0600

    added style sheet to play with

--- index.view.php
+++ index.view.php
@@ -7,89 +7,31 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="style.css" / >
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" 
+        crossorigin="anonymous">
 
     </head>
     <body>
-        <header>   
+        <header>  
             <ul>
-            
-                <?php foreach ($names as $name) :?>
-                        
-                    <li><?= $name ?></li>
-
-                <?php endforeach;?>
                 
-                <br>
-
-                <?php
-                    foreach ($names as $name) {
-                        echo "<li>$name</li>";
-
-                    }
-                ?>
-
-                <br>
-                <p>List of Animals</p>
-                <br>
-
-
-                <?php foreach ($animals as $animal) :?>
-
-                    <li><?= $animal ?></li>
+                <?php foreach ($names as $name) :?>
+                                    
+                        <li><?= $name ?></li>
 
                 <?php endforeach;?>
-
-                <br>
-
-                <?php
-                    foreach ($animals as $animal) {
-                        echo "<li>$animal</li>";
-
-                    }
-                ?>
-
-
-
-            </ul>
-            
-            <ul>
-                <?php foreach ($person as $key => $feature)  : ?>
-
-                    <li><strong><?= $key; ?></strong><br> <?= $feature; ?></li>
-
-                <?php endforeach; ?>
-
-
-            </ul>
-
-            <h1> Task for the day</h1>
-
-            <!-- <ul>
-                <?php foreach ($task as $heading => $value) : ?>
-                    <li>
-                        <strong><?= ucwords($heading); ?>:</strong> <?= $value; ?>
-                    </li>
-                <?php endforeach; ?>
-
-            </ul> -->
-
-            <ul>
-                <li>
-                    <strong>Name: </strong> <?= $task['title']; ?>
-                </li>                        
-
-                <li>
-                    <strong>Due Date: </strong> <?= $task['due']; ?>
-                </li>  
-
-                <li>
-                    <strong>Personal Responsible: </strong> <?= $task['assigned_to']; ?>
-                </li>  
-
-                <li>
-                    <strong>Status: </strong> <?= $task['completed'] ? 'Complete' : 'Incomplete'; ?>
-                </li>  
-            </ul>
+               
+            </ul>            
         </header>
+        <section> 
+
+            <?php
+                foreach ($names as $name) {
+                    echo "<li>$name</li>";
+                }
+            ?>
+
+        </section>
     </body>
 </html>
