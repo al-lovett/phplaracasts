c85babda8e467f9875a1a53c857630f614d72d37
alovett <alovett1215@gmail.com>
2020-04-13 09:46:56 -0600

    learning more on classes and functions

---
+++ function.php
@@ -0,0 +1,22 @@
+<?php
+
+  $species = ['gal fed', 'st germain', 'dragons'];
+
+
+  function dd($data){
+
+    echo '<pre>';
+    die(var_dump($data));
+    echo '<pre>';
+  }
+
+    // call function with arg 1,2,3
+
+    // dumper('hello', 'big star', 'moon');
+
+
+  dd('species string');
+
+
+?>
+
---
+++ vari.php
@@ -0,0 +1,35 @@
+<?php
+
+  $user = "al";
+
+  if ($user == "me") {
+
+    echo "Hello Alien";
+
+  } else {
+
+    echo "Wrong Planet DingDong";
+    
+  }
+
+  echo "<br><br><br>";
+
+
+  $age = 5;
+
+  if ($age >= 18) {
+
+    echo "Age is Important";
+
+  } else {
+
+    echo "No Its Not";
+
+  }
+
+
+
+
+
+
+?>
